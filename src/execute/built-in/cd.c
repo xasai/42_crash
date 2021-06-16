f06be765bc04622731299ad7d8710bf65efb2bbb
GGWP-grats <grats420@gmail.com>
2021-06-16 15:50:54 +0300

    cd justify

--- src/execute/built-in/cd.c
+++ src/execute/built-in/cd.c
@@ -3,16 +3,11 @@
 int	crash_cd(t_dlist *cmd, t_shell *crash)
 {
 	char	*path;
-	uint8_t	arg_counter;
 	
-	arg_counter = 0;
-	while (cmd->arg[arg_counter])
+	if (cmd->arg[2])
 	{
-		if (arg_counter++ > 1)
-		{
-			putstr_fd("crash: cd: too many arguments\n", STDERR_FILENO);
-			return (RETURN_FAILURE);
-		}
+		putstr_fd("crash: cd: too many arguments\n", STDERR_FILENO);
+		return (RETURN_FAILURE);
 	}
 	if (cmd->arg[1])
 		path = cmd->arg[1];
