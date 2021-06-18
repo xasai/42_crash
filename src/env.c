9e9caab35346cb0d76ec3ff95872042999223138
GGWP-grats <grats420@gmail.com>
2021-06-18 14:11:07 +0300

    ff

---
+++ src/env.c
@@ -0,0 +1,12 @@
+#include "minishell.h"
+
+char	*crash_getenv(char *name, char **envp)
+{
+	//size_t	idx;
+	//size_t	len;
+
+	//idx = 0;
+	//len = ft_strlen(name);
+	(void)envp;(void)name;
+	return (NULL);
+}
