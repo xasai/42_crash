728dbaa592f5787f19967ee2e60128279cc356f3
kreker008 <https://kreker008@github.com>
2021-07-03 03:02:43 +0400

    more env fix

--- src/pars/expand_env.c
+++ src/pars/expand_env.c
@@ -4,7 +4,7 @@ int	get_envkey_len(char *line)
 {
 	int envkey_len;
 
-    envkey_len = 0;
+    envkey_len = 1;
 	if (ft_isalpha(line[envkey_len]) || line[envkey_len] == '_')
 		++envkey_len;
 	else if (line[envkey_len] == '?')
