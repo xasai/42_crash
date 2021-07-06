321ac29c9285fe2a9e197009fafe2b65bab3b33d
GGWP-grats <grats420@gmail.com>
2021-07-07 00:37:31 +0300

    readline.c empty string memleak fixed

--- src/readline.c
+++ src/readline.c
@@ -7,7 +7,7 @@ static	void	_int_handler(int signum)
 	signal(signum, _int_handler);
 	write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
-	//rl_replace_line("", 0);
+	rl_replace_line("", 0);
 	rl_redisplay();
 	g_sh->exit_status = 0x80 + signum;
 }
@@ -27,18 +27,16 @@ static	void	_set_sigdefault(void)
 char	*crash_readline(void)
 {
 	char		*line;
-	size_t		len;
-	static char	*prev_line = "";
+	static char	*prev_line;
 
 	_set_sigdefault();
 	_set_sighandlers();
 	line = readline(g_sh->prompt);
 	if (line == NULL)
 		exit_message("exit", EXIT_SUCCESS);
-	len = ft_strlen(line);
-	if (len && ft_strncmp(line, prev_line, len + 1))
+	if (prev_line && ft_strcmp(line, prev_line))
 		add_history(line);
-	if (*prev_line != '\0')
+	if (prev_line)
 		free(prev_line);
 	prev_line = line;
 	return (line);
